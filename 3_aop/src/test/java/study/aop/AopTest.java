ce61457a0a92f49162a35571ba9d803c35f0a7ca
profoundsea25 <profoundsea25@gmail.com>
2022-04-24 14:30:26 +0900

    10. 스프링 AOP 구현 ~11

---
+++ 3_aop/src/test/java/study/aop/AopTest.java
@@ -0,0 +1,43 @@
+package study.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import study.aop.order.OrderRepository;
+import study.aop.order.OrderService;
+import study.aop.order.aop.AspectV1;
+import study.aop.order.aop.AspectV2;
+
+@Slf4j
+//@Import(AspectV1.class)
+@Import(AspectV2.class)
+@SpringBootTest
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
