9c1ccd18e0f06397da012a6e545a1b050a3b8863
profoundsea25 <profoundsea25@gmail.com>
2022-04-28 20:03:33 +0900

    11. 스프링 AOP - 포인트컷 ~28

---
+++ 3_aop/src/test/java/study/aop/pointcut/WithinTest.java
@@ -0,0 +1,53 @@
+package study.aop.pointcut;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import study.aop.member.MemberServiceImpl;
+
+import java.lang.reflect.Method;
+
+public class WithinTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact() {
+        pointcut.setExpression("within(study.aop.member.MemberServiceImpl)");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinStar() {
+        pointcut.setExpression("within(study.aop.member.*Service*)");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinSubPackage() {
+        pointcut.setExpression("within(study.aop..*)");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("타겟의 타입에만 직접 적용, 인터페이스를 선정하면 안 된다.")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(study.aop.member.MemberService)");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스를 선정 가능.")
+    void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* study.aop.member.MemberService.*(..))");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+}
