813c2512cc7d584af777e8c9b76a9a734a89255c
profoundsea25 <profoundsea25@gmail.com>
2022-04-30 22:46:45 +0900

    13. 스프링 AOP - 실무 주의사항 ~13

---
+++ 3_aop/src/test/java/study/aop/internalcall/CallServiceV1Test.java
@@ -0,0 +1,22 @@
+package study.aop.internalcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import study.aop.internalcall.aop.CallLogAspect;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV1Test {
+
+    @Autowired
+    CallServiceV1 callServiceV1;
+
+    @Test
+    void external() {
+        callServiceV1.external();
+    }
+}
