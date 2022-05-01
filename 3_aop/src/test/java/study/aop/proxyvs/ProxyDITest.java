4440dad13e7fa680fc4efc75fc6a28875b9e2b4c
profoundsea25 <profoundsea25@gmail.com>
2022-05-01 22:27:38 +0900

    13. 스프링 AOP - 실무 주의사항 ~End

--- 3_aop/src/test/java/study/aop/proxyvs/ProxyDITest.java
+++ 3_aop/src/test/java/study/aop/proxyvs/ProxyDITest.java
@@ -11,7 +11,8 @@ import study.aop.proxyvs.code.ProxyDIAspect;
 
 @Slf4j
 //@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시, DI 예외 발생
-@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 프록시, 성공
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 프록시, 성공
+@SpringBootTest // 설정을 따로 안 하면 spring.aop.proxy-target-class=true 이다. 따라서 CGLIB 프록시를 사용하고, 테스트를 성공한다.
 @Import(ProxyDIAspect.class)
 public class ProxyDITest {
 
